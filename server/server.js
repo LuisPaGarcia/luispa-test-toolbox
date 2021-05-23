3248c1c0ba5656d4e226cf56c678e18b92d69bb1
LuisPa Garcia <diganluispa@gmail.com>
2021-05-22 20:03:38 -0600

    refactor: Adding general comments

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+// Start express app
 const app = express();
 
+// Import config and router
 const config = require("./config");
 const inboundRouter = require("./inbound/inbound.router");
 
+// Disable headers
 app.disable("x-powered-by");
+
+// Add global middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Apply first API route
 app.use("/api/inbound", inboundRouter);
 
+// Start core function
 const start = async () => {
   try {
     app.listen(config.port, () => {
